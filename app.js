f57e2ea99f2fca5e7b2d0c47eba8d73daedce528
MacJ13 <jacdim02@gmail.com>
2023-03-03 20:00:58 +0100

    Create generateBookRowELement function
    Assign and call function inside addBookToLibrary function

--- app.js
+++ app.js
@@ -28,11 +28,42 @@ function addBookToLibrary(e) {
 
   console.log(book);
 
+  // create 'book row' element with object property values
+  const bookRowEL = generateBookRowELement(book);
+
   // add book object to array
   myLibrary.push(book);
 
+  // display created element inside 'book-list' class element
+  booklist.appendChild(bookRowEL);
   // generateBookElement(book);
   // myLibrary.push(book);
+}
+
+function generateBookRowELement(book) {
+  // Create li html element and assign "book-row" class
+  const row = document.createElement("li");
+  row.className = "book-row";
+
+  // loop over object book properties
+  for (const property in book) {
+    // create html for property and assign property to class name
+    const item = document.createElement("span");
+    item.className = property;
+
+    // assign textContent element to object property value
+
+    if (property === "read") {
+      item.textContent = book[property] ? "read" : "not read";
+      book[property] || item.classList.add("uncompleted");
+    } else {
+      item.textContent = book[property];
+    }
+
+    row.insertAdjacentElement("beforeend", item);
+  }
+
+  return row;
 }
 
 // sumbmit form event
