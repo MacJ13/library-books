f22e7098d30605c398d99b290c0337d1a6ed7384
MacJ13 <jacdim02@gmail.com>
2023-03-03 19:15:10 +0100

    Create addBookToLibrary function

--- app.js
+++ app.js
@@ -10,3 +10,27 @@ function Book(title, author, pages, read) {
   this.pages = pages;
   this.read = read;
 }
+
+function addBookToLibrary(e) {
+  // stop default behavior of submit event
+  e.preventDefault();
+
+  // create variables with input value
+  const title = document.getElementById("title").value;
+  const author = document.getElementById("author").value;
+  const pages = document.getElementById("pages").value;
+
+  // create value depend on checkbox is checked
+  const read = checkbox.checked ? true : false;
+
+  // create new Book object
+  const book = new Book(title, author, +pages, read);
+
+  console.log(book);
+
+  // add book object to array
+  myLibrary.push(book);
+
+  // generateBookElement(book);
+  // myLibrary.push(book);
+}
